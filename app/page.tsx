6d6c8d9419e2a8225bf89110cdc048ac93f70810
Septikiwir <wahyuseptapramudya@students.amikom.ac.id>
2026-04-26 17:12:15 +0700

    feat: add gift and payment section components to wedding invitation page

--- app/page.tsx
+++ app/page.tsx
@@ -783,7 +783,7 @@ export default function Home() {
               color: 'var(--gold)',
               marginBottom: '1rem'
             }}>
-              Assalaamu&apos;alaikum Wr. Wb.
+              Assalamu&apos;alaikum Wr. Wb.
             </p>
             <div className="ornament-center reveal reveal-fade">
               <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
